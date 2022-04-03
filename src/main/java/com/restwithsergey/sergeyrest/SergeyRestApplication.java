0a8cfaa0fdf6c4e626f64652a5d750ac37832459
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-04-03 12:30:22 +0100

    modified pom.xml to for war packaging and deployment to apache tomcat

--- src/main/java/com/restwithsergey/sergeyrest/SergeyRestApplication.java
+++ src/main/java/com/restwithsergey/sergeyrest/SergeyRestApplication.java
@@ -4,12 +4,18 @@ import com.restwithsergey.sergeyrest.configuration.util.AppProperties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class SergeyRestApplication {
+public class SergeyRestApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SergeyRestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SergeyRestApplication.class, args);
